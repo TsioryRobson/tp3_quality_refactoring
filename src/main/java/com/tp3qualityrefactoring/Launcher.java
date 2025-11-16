8ec0ab2927da236b4cc2d92116a72496e963d84a
Dania Midera <daniamidera@gmail.com>
2025-11-16 22:42:30 +0100

    tp3_quality_refactoring: Add first GUI

---
+++ src/main/java/com/tp3qualityrefactoring/Launcher.java
@@ -0,0 +1,9 @@
+package com.tp3qualityrefactoring;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
